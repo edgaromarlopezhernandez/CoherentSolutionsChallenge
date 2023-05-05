e6517f3f2db4aa13667f0788722027034f152848
Edgar Omar Lopez Hernandez <edgar.lopezh@agilethought.com>
2023-05-05 14:46:08 -0600

    Initial commit - Project created from Spring Initializer

---
+++ src/main/java/com/coherentSolutions/challenge/ChallengeToBeHiredApplication.java
@@ -0,0 +1,13 @@
+package com.coherentSolutions.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeToBeHiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeToBeHiredApplication.class, args);
+	}
+
+}
